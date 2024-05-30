ff5d1fa0978b283714aec1678c4f92b7d306a750
cthegost <akhavkin@medlinesoft.ru>
2024-05-30 21:44:50 +0300

    lol

--- api/index.js
+++ api/index.js
@@ -6,4 +6,9 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+});
+
 module.exports = server;
